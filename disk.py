ef7f4326e7a82b8c6ee7c8201f371164383bd24a
syedibrahimsaeed <ssaeed04@students.cs.ubc.ca>
2025-09-08 17:48:54 -0700

    Feature: implement final_disk_speed

--- disk.py
+++ disk.py
@@ -13,3 +13,20 @@ def final_disk_speed(height: float, length: float, incline: float, mass: float, 
     :param radius: the radius of the disk (meters)
     :return: the speed of the disk (m/s)
     """
+
+    g = 9.81
+    # intial potential energy
+    PE = mass * g * height
+    # normal force that gets applied
+    normal_force = mass * g * np.cos(np.radians(incline))
+    # force applied by friction
+    friction_force = friction * normal_force
+    # calculate total work done
+    work = friction_force * length
+    # calculate net energy leftover
+    net_energy = PE - work
+
+    if net_energy <= 0:
+        return 0
+    else:
+        np.sqrt(4 * net_energy / (3 * mass))
